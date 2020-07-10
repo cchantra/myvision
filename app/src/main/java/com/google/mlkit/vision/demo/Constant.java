5d3916ac81a39e25822bc37b19df2b16720f9c12
chantanasmacbook <chantrapornchai@gmail.com>
2020-07-10 08:48:46 +0700

    change label processor

---
+++ app/src/main/java/com/google/mlkit/vision/demo/Constant.java
@@ -0,0 +1,9 @@
+package com.google.mlkit.vision.demo;
+
+
+public final class Constant {
+    public static final int AGE_OPTION = 1;
+    public static final int FACE_OPTION = 2;
+    public static final int EXP_OPTION = 3;
+    public static final int GENDER_OPTION = 4;
+}
