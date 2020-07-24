b4a73c06d1eaa597b0f9656398be6dbb667a2d9c
chantanasmacbook <chantrapornchai@gmail.com>
2020-07-25 06:34:09 +0700

    add test API for analysis

--- app/src/main/java/com/google/mlkit/vision/demo/facedetector/FaceDetectorProcessor.java
+++ app/src/main/java/com/google/mlkit/vision/demo/facedetector/FaceDetectorProcessor.java
@@ -19,6 +19,7 @@ package com.google.mlkit.vision.demo.facedetector;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.PointF;
+import android.graphics.RectF;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -28,10 +29,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.demo.BitmapUtils;
 import com.google.mlkit.vision.demo.Constant;
+import com.google.mlkit.vision.demo.FaceAPIUtil;
 import com.google.mlkit.vision.demo.GraphicOverlay;
 import com.google.mlkit.vision.demo.LivePreviewActivity;
 import com.google.mlkit.vision.demo.Logger;
 import com.google.mlkit.vision.demo.VisionProcessorBase;
+import com.google.mlkit.vision.demo.constantURL;
 import com.google.mlkit.vision.demo.tflite.Classifier;
 import com.google.mlkit.vision.face.Face;
 import com.google.mlkit.vision.face.FaceDetection;
@@ -39,9 +42,18 @@ import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
 import com.google.mlkit.vision.face.FaceLandmark;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * Face Detector Demo.
@@ -50,6 +62,7 @@ public class FaceDetectorProcessor extends VisionProcessorBase<List<Face>> {
 
     private static final String TAG = "FaceDetectorProcessor";
     private static final Logger LOGGER = new Logger();
+    private static final int MAX_RESULTS = 3;
 
     private final FaceDetector detector;
 
@@ -110,36 +123,100 @@ public class FaceDetectorProcessor extends VisionProcessorBase<List<Face>> {
     @Override
     protected void onSuccess(@NonNull List<Face> faces, @NonNull Bitmap bitmap, @NonNull GraphicOverlay graphicOverlay) throws MalformedURLException {
 
-        int i=0;
+
         if (mode == Constant.TEST_JSON) {
             final long startTime = SystemClock.uptimeMillis();
+            Bitmap [] bma = {bitmap};
+            List<List<Classifier.Recognition>> results =  new ArrayList<List<Classifier.Recognition>>();
 
-            final List<List<Classifier.Recognition>> results =
-                    classifier.recognizeImageAll(bitmap);
-            long lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
-            LOGGER.v("Detect: %s", results);
-            System.out.println(i + " xxx" + results);
+            //classifier.recognizeImageAll(bitmap, results);
 
-            for (Face face : faces) {
+            FaceAPIUtil asyncTask = new FaceAPIUtil(new URL(constantURL.FaceURL), output -> {
+                //Here you will receive the result fired from async class
+                //of onPostExecute(result) method.
 
-                i++;
-                graphicOverlay.add(new FaceGraphic(graphicOverlay, face, i)); // must draw itself
+                Log.e("output", output);
 
-                logExtrasForTesting(face);
+                // parse : {'emotion': [['sad'], ['neutral']], 'detail': [[[1, 2, 3, 4], [0.56]], [[1, 2, 3, 5], [0.44]]]}
+                JSONObject json = new JSONObject(output);
+                JSONArray emotionArray = (JSONArray) json.get("emotion");
+                JSONArray detailArray = (JSONArray) json.get("detail");
+
+                for (int i=0; i < emotionArray.length(); i++) { // for each face
+                    System.out.println(emotionArray.get(i));
+                    System.out.println(detailArray.get(i));
+
+                    Map<String, Object []> labeledProbability =  new HashMap<String, Object[]>();
 
 
-                LivePreviewActivity imageView = (LivePreviewActivity) context;
-               // imageView.showObjectID(i);
-               // imageView.showResultsInBottomSheet(results.get(i));
+                    JSONArray plist = (JSONArray) emotionArray.get(i);
+                    JSONArray dlist = (JSONArray) detailArray.get(i);
 
-            }
+                    //String [] plist = (String []) obj; // plist[i] is array for each face
+                    //Object [] dlist = (Object [] ) detailArray.get(i); // array for each face
+                    for (int j=0; j < plist.length() ; j++) { // list of predictions
+                        Log.e("class", (String) plist.get(j));
+
+                        //Object [] ddlist = (Object[]) dlist[j];
+                        JSONArray  loc_array = (JSONArray)dlist.get(0);
+                        System.out.println(loc_array);
+
+                        JSONArray confs = (JSONArray)  dlist.get(1);
+
+
+                        System.out.println("list of confs");
+                        System.out.println(confs);
+                        Log.e("prob" ,   confs.get(j).toString());
+                        // insert into hashMap
+                        Object [] values = new Object[2];
+                        values[0] = (double ) confs.get(j);
+                        values[1] = new RectF( (int) loc_array.get(0), (int) loc_array.get(1),   (int)loc_array.get(0)+(int)loc_array.get(2), (int)loc_array.get(1)+ (int)loc_array.get(3));
+                        labeledProbability.put((String) plist.get(j),values);
+
+
+                    }
+                    //recogListAll.add( getTopKProbabilityWithLocation(labeledProbability));
+                    results.add( getTopKProbabilityWithLocation(labeledProbability));
+                }
+
+
+                System.out.println("added "+results.size());
+                LOGGER.v("Detect: %s", results);
+                System.out.println(  " xxx" + results);
+
+                //for (Face face : faces) {
+                int i=0;
+                for (Face face : faces) {
+                    if (results.size() == 0)
+                        break;
+                    List<Classifier.Recognition> result = results.get(i);
+                    i++;
+                    graphicOverlay.add(new FaceGraphic(graphicOverlay, face, i, result )); // must draw itself
+
+                    logExtrasForTesting(face);
+
+
+                    LivePreviewActivity imageView = (LivePreviewActivity) context;
+                    imageView.showObjectID(i);
+                    imageView.showResultsInBottomSheet(result);
+
+                }
+
+            });
+
+            asyncTask.execute(bma);
+
+
+
+
         }
         else {
+            int i =0;
             for (Face face : faces) {
 
                 i++;
 
-                graphicOverlay.add(new FaceGraphic(graphicOverlay, face, i));
+                graphicOverlay.add(new FaceGraphic(graphicOverlay, face, i, null));
 
 
                 final long startTime = SystemClock.uptimeMillis();
@@ -162,6 +239,36 @@ public class FaceDetectorProcessor extends VisionProcessorBase<List<Face>> {
     }
 
 
+
+    private static List<Classifier.Recognition> getTopKProbabilityWithLocation(Map<String, Object[] > labelProb) {
+        // Find the best classifications.
+
+        PriorityQueue<Classifier.Recognition> pq =
+                new PriorityQueue<>(
+                        MAX_RESULTS,
+                        new Comparator<Classifier.Recognition>() {
+                            @Override
+                            public int compare(Classifier.Recognition lhs, Classifier.Recognition rhs) {
+                                // Intentionally reversed to put high confidence at the head of the queue.
+                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                            }
+                        });
+
+        for (Map.Entry<String, Object[]> entry : labelProb.entrySet()) {
+            Object [] values = entry.getValue();
+            double conf = (double) values[0];
+            pq.add(new Classifier.Recognition("" + entry.getKey(), entry.getKey(), (float) conf, (RectF) values[1]));
+            System.out.println(entry.getValue());
+        }
+
+        final ArrayList<Classifier.Recognition> recognitions = new ArrayList<>();
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        // System.out.println(pq.size());
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+        return recognitions;
+    }
     private static void logExtrasForTesting(Face face) {
         if (face != null) {
             Log.v(MANUAL_TESTING_LOG, "face bounding box: " + face.getBoundingBox().flattenToString());
